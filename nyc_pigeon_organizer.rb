f4c04874e49175b60c4c5c727defe89c024cf831
Sarah H <sarahrose.henderson@gmail.com>
2019-09-03 20:18:23 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,10 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+new_pigeon_data = {}
+data.each do |attribute_category, attribute_data_hash|
+  attribute_data_hash.each do |attribute, name_array|
+    name_array.each do |name|
+      new_pigeon_data[name] = {:color => [], :gender => [], :lives => []}
+end
+end      
 end
